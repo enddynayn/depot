9e3841007788e16ae7e6ed391dee18022605148d
Enddy Dumbrique <enddynayn@hotmail.com>
2013-10-17 22:31:55 -0700

    add model test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
-
 class ProductTest < ActiveSupport::TestCase
+	fixtures :products
 	test "product attributes must not be empty" do 
   		product = Product.new
   		assert product.invalid?
@@ -28,6 +28,30 @@ class ProductTest < ActiveSupport::TestCase
 		assert product.valid?
 	end
 
-
+	def new_product(image_url)
+		Product.new(title:       "My Boot Title",
+		 			description: "yyy",
+		 			price:        1,
+		 			image_url:    image_url)
+	end
+	test "image url" do
+		ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg 
+			http://a.b.c/c/y/zfred.gif }
+		bad = %w{ fred.doc fred.gif/more fred.gif.more }
+		ok.each do |name|
+			assert new_product(name).valid?, "#{name} should be valid"
+		end
+		bad.each do |name|
+			assert new_product(name).invalid?, "#{name} shouldn't be valid"
+		end
+	end
+	test "product is not valid without a unique title" do 
+		product = Product.new(	title:     		products(:ruby).title,
+								description: 	"yyy",
+								price:       	"fred.gif" )
+		assert product.invalid?
+		assert_equal ["has already been taken"], product.errors[:title]
+	end
+	
 
 end
