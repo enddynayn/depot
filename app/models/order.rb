c274d45eea9c4169580b2732ebe9544cddc3b2f8
Enddy Dumbrique <enddynayn@hotmail.com>
2013-10-28 22:58:25 -0700

    modified form to select only from a list of payment types

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,3 @@
 class Order < ActiveRecord::Base
+	PAYMENT_TYPES = ["Check", "Credict card", "Purchase order" ]
 end
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -5,16 +5,30 @@ class OrdersControllerTest < ActionController::TestCase
     @order = orders(:one)
   end
 
+  test "requires item in cart" do 
+    get :new
+    assert_redirected_to store_path
+    assert_equal flash[:notice], 'Your cart is empty'
+  end
+
+  test "should get new" do 
+    item = LineItem.new
+    item.build_cart
+    item.product = products(:ruby)
+    item.save!
+    session[:cart_id] = item.cart.id
+    get :new
+    assert_response :success
+  end
+
+
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:orders)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+
 
   test "should create order" do
     assert_difference('Order.count') do
