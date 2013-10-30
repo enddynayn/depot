5f232e94e1de3309fbc914ad8aa686ccfdebe81e
Enddy Dumbrique <enddynayn@hotmail.com>
2013-10-29 17:30:18 -0700

    add atom.builder and product who_bought on member

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Depot::Application.routes.draw do
   resources :orders
-
   resources :line_items
-
   resources :carts
 
   root "store#index", as: 'store'
-  resources :products
+  
+  resources :products do 
+    get :who_bought, on: :member
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
