a38f433e7ac0c3626076dfa02b70a720f8c6ed70
Enddy Dumbrique <enddynayn@hotmail.com>
2013-10-31 15:28:06 -0700

    limit access by creating a authorize callback in application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Depot::Application.routes.draw do
-  get "admin/index"
-  get "sessions/new"
-  get "sessions/create"
+  get 'admin' => 'admin#index'
+  controller :sessions do 
+    get    'login'  => :new 
+    post   'login'  => :create 
+    delete 'logout' => :destroy
+  end
+  
+  
   get "sessions/destroy"
   resources :users
-
   resources :orders
   resources :line_items
   resources :carts
 
   root "store#index", as: 'store'
-  
   resources :products do 
     get :who_bought, on: :member
   end
